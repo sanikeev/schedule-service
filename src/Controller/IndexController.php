aa01e96773b33dd3c16d6bf768de05b666403163
Sergey Anikeev <keshbuk@gmail.com>
2018-12-24 19:16:55 +0300

    init commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     * @Route("/admin/{vueRouting}", name="admin")
+     * @Route("/admin/{vueRouting}/{vueRouting1}")
+     * @Route("/admin/{vueRouting}/{vueRouting1}/{vueRouting2}")
+     * @Route("/{vueRouting}", name="index")
+     */
+    public function index()
+    {
+        return $this->render("base.html.twig", []);
+    }
+}
