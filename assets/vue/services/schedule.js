f7066231ee53d546ec9f5423d2974552c0cf27f8
Sergey Anikeev <keshbuk@gmail.com>
2018-12-25 21:22:21 +0300

    filter for list on client

--- assets/vue/services/schedule.js
+++ assets/vue/services/schedule.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
+import moment from "moment";
 
 export default {
   list: function (params) {
-    return axios.get('/api/schedule', params).then(data => data.data);
+    return axios.get('/api/schedule', {
+      params: {
+        start: moment(params.start).format('YYYY-MM-DD'),
+        end: moment(params.end).format('YYYY-MM-DD'),
+        page: params.page
+      }
+    }).then(data => data.data);
   },
   arrival: function (params) {
     return axios.post('/api/schedule/arrival', params).then(data => data.data)
--- assets/vue/views/Index.vue
+++ assets/vue/views/Index.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="container">
+        <div class="container">
+            <div class="row" style="margin: 15px">
+                <form>
+                    <div class="form-row">
+                        <div class="col">
+                            <datepicker v-model="query.start" @input="filter()" format="yyyy-MM-dd"
+                                        placeholder="From"></datepicker>
+                        </div>
+                        <div class="col">
+                            <datepicker v-model="query.end" @input="filter()" format="yyyy-MM-dd"
+                                        placeholder="To"></datepicker>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
         <div class="row">
             <table class="table">
                 <tr>
@@ -17,23 +33,49 @@
                     <td>{{ s.ended_at }}</td>
                 </tr>
             </table>
+            <nav>
+                <paginate
+                        :page-count="totalPages"
+                        :click-handler="paginate"
+                        :prev-text="'Prev'"
+                        :next-text="'Next'"
+                        :container-class="'pagination'"
+                        :page-link-class="'page-link'"
+                        :prev-class="'age-item'"
+                        :prev-link-class="'page-link'"
+                        :next-class="'age-item'"
+                        :next-link-class="'page-link'"
+                        :page-class="'page-item'">
+                </paginate>
+            </nav>
         </div>
     </div>
 </template>
 
 <script>
-    import scheduleService from '../services/schedule'
+  import scheduleService from '../services/schedule'
+  import Datepicker from 'vuejs-datepicker';
+  import Paginate from 'vuejs-paginate'
+  import moment from 'moment'
+
+
   export default {
     name: "Index",
+    components: {Datepicker, Paginate},
     data: function () {
       return {
         list: [],
-        totalPages: null,
-        pages: []
+        totalPages: 0,
+        pages: [],
+        query: {
+          start: (new Date()).setMonth((new Date()).getMonth() - 1),
+          end: new Date(),
+          page: 1
+        }
       }
     },
     beforeMount() {
-      scheduleService.list({}).then(data => {
+      scheduleService.list(this.query).then(data => {
         this.list = data.data
         this.totalPages = data.totalPages
         for (let index = 1; index <= data.totalPages; index++) {
@@ -42,9 +84,24 @@
       })
     },
     methods: {
-        paginate() {
-
+      paginate(page) {
+        this.query.page = page
+        scheduleService.list(this.query).then(data => {
+          this.list = data.data
+          this.totalPages = data.totalPages
+        })
+      },
+      filter() {
+        let start = this.query.start.getTime();
+        let end = this.query.end.getTime();
+        if (start > end) {
+          this.query.start = this.query.end
         }
+        scheduleService.list(this.query).then(data => {
+          this.list = data.data
+          this.totalPages = data.totalPages
+        })
+      }
     }
   }
 </script>
